c968657ad49b22ad5ebdcb43b3496f91f970a2e3
GL <g@gleech.org>
2017-08-30 21:58:41 +0100

    Reformat in lieu of actual view/controller

--- scripts/comput/ComputerSelector.py
+++ scripts/comput/ComputerSelector.py
@@ -19,14 +19,6 @@ def refresh(name) :
     set_computer(firstComputer)
 
 
-def reset() :
-    for v in fieldModel:
-        for f in document.getElementsByName(v) :
-            f.checked = False
-                
-    set_computer(nullComputer)
-
-
 def filter_candidates(matches, criteria) :
     for key, criterion in criteria.items() :
         matches = [ m for m in matches if m[key] == criterion ] 
@@ -59,6 +51,91 @@ def get_computer(data, criteria) :
         return get_earliest(matches, sortCmp)
     else:
         return nullComputer
+
+
+# First handle some special cases: e.g. analogs have no base.
+# UX is terrible without this
+def infer_predicates(data, name) :
+    if name == 'programmables' :
+        data = constrain_single_program(data)
+    elif name == 'universal' :
+        data = constrain_turing(data)
+    elif name == 'transistorised' :
+        data = constrain_transistor(data)
+    elif name == 'stored' :
+        data = constrain_stored(data)
+    elif name == 'gui' :
+        data = constrain_gui(data)
+    elif name == 'base' :
+        data = constrain_digital(data)
+    elif name == 'representation' :
+        data = constrain_analogue(data)
+
+    return data
+
+
+def constrain(id, isChecked=True) :
+    document.getElementById(id).checked = isChecked
+
+
+def constrain_analogue(data):
+    elements = document.getElementsByName(BASE)
+
+    if data['representation'] == ANALOG :
+        for el in elements :
+            el.checked = False
+            el.disabled = True
+    else :
+        for el in elements :
+            el.disabled = False
+
+    return data
+
+
+def constrain_digital(data) :
+    if data['base'] is not "" :
+        constrain(digId)
+        data['representation'] = DIGITAL
+    return data
+
+
+def constrain_single_program(data) :
+    if data['programmables'] == '' : 
+        constrain(specialId)
+        data['universal'] = SPECIAL
+        constrain('nonstor')
+        data['stored'] = ''
+        constrain(generalId, False)
+     
+    return data
+
+
+def constrain_turing(data) :
+    if data['universal'] == TURING :
+        constrain(programId)
+        data['programmables'] = PROG_YES
+    return data
+
+
+def constrain_transistor(data) :
+    if data['transistorised'] == TRANSIST :
+        constrain(electroId)
+        data['signals'] = ELECTRO
+    return data
+    
+
+def constrain_stored(data) :
+    if data['stored'] == STORED :
+        constrain(programId)
+        data['programmables'] = PROG_YES
+    return data
+
+
+def constrain_gui(data) :
+    if data['gui'] == GUI :
+        constrain(electroId)
+        data['signals'] = ELECTRO
+    return data
 
 
 #  Only once
@@ -98,86 +175,10 @@ def hide_intro_text() :
     document.getElementById(intro).style.display = "none"
 
 
-# First handle some special cases: e.g. analogs have no base.
-# UX is terrible without this
-def infer_predicates(data, name) :
-    if name == 'programmables' :
-        data = constrain_single_program(data)
-    elif name == 'universal' :
-        data = constrain_turing(data)
-    elif name == 'transistorised' :
-        data = constrain_transistor(data)
-    elif name == 'stored' :
-        data = constrain_stored(data)
-    elif name == 'gui' :
-        data = constrain_gui(data)
-    elif name == 'base' :
-        data = constrain_digital(data)
-    elif name == 'representation' :
-        data = constrain_analogue(data)
 
-    return data
-
-
-def constrain(id) :
-    document.getElementById(id).checked = True
-
-
-def constrain_analogue(data):
-    elements = document.getElementsByName(BASE)
-
-    if data['representation'] == ANALOG :
-        for el in elements :
-            el.checked = False
-            el.disabled = True
-    else :
-        for el in elements :
-            el.disabled = False
-
-    return data
-
-
-def constrain_digital(data) :
-    if data['base'] is not "" :
-        constrain(digId)
-        data['representation'] = DIGITAL
-    return data
-
-
-def constrain_single_program(data) :
-    if data['programmables'] == '' : 
-        constrain(specialId)
-        data['universal'] = SPECIAL
-        constrain('nonstor')
-        data['stored'] = ''
-        document.getElementById(generalId).checked = False
-     
-    return data
-
-
-def constrain_turing(data) :
-    if data['universal'] == TURING :
-        constrain(programId)
-        data['programmables'] = PROG_YES
-    return data
-
-
-def constrain_transistor(data) :
-    if data['transistorised'] == TRANSIST :
-        constrain(electroId)
-        data['signals'] = ELECTRO
-    return data
-    
-
-def constrain_stored(data) :
-    if data['stored'] == STORED :
-        constrain(programId)
-        data['programmables'] = PROG_YES
-    return data
-
-
-def constrain_gui(data) :
-    if data['gui'] == GUI :
-        constrain(electroId)
-        data['signals'] = ELECTRO
-    return data
+def reset() :
+    for field in fieldModel:
+        for dom in document.getElementsByName(field) :
+            dom.checked = False
+                
+    set_computer(nullComputer)
