82c2e4f4042f792d9e98a669be614abe0184ae2e
Gavin L <gleech@protonmail.com>
2023-09-27 23:49:48 +0200

    wordcount

---
+++ _plugins/thousands-separated-filter.rb
@@ -0,0 +1,9 @@
+module Jekyll
+  module ThousandsSeparatedFilter
+    def thousands_separated(input, separator=",")
+      input.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{separator}")
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ThousandsSeparatedFilter)
