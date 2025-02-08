c80ad4f847ca21aa49524b2366c9e88cdc553aab
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 18:54:38 +0000

    Add multistage build

--- scripts/serve-docker.sh
+++ scripts/serve-docker.sh
@@ -4,6 +4,6 @@ set -e
 # Make sure this script is run from the project root.
 
 echo "Building Docker image..."
-docker build --progress=plain -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
+docker build --progress=plain --platform=linux/amd64 -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
 echo "Running Docker container..."
 docker run -p 4000:4000 argmin-gravitas/latest
