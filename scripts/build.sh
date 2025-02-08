bd7ab6c7b5c8862a429d1682e56f5684e7dd8d8e
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 21:32:01 +0000

    Use same dockerfile for build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Building Docker image for static site generation..."
-docker build --platform=linux/amd64 -t argmin-gravitas/jekyll-build -f Dockerfiles/Dockerfile.build .
+docker build --platform=linux/amd64 -t argmin-gravitas/jekyll-build -f Dockerfiles/Dockerfile.jekyll .
 
 echo "Generating static site..."
 # Mount your local "./_site" directory to the container's /srv/jekyll/_site directory.
 mkdir -p _site
-docker run --platform=linux/amd64 -v "$(pwd)/_site:/srv/jekyll/_site" argmin-gravitas/jekyll-build
+docker run --platform=linux/amd64 -v "$(pwd)/_site:/srv/jekyll/_site" argmin-gravitas/jekyll-build bundle exec jekyll build
 
 echo "Site built successfully into the ./_site directory."
