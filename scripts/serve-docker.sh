6a06c0aeebbe17fa27c3ff7aafc11db5f605bd3c
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 17:51:57 +0000

    Fix docker image name

--- scripts/serve-docker.sh
+++ scripts/serve-docker.sh
@@ -2,5 +2,5 @@
 
 # Make sure this script is run from the project root.
 
-docker build -t argmin-gravitas/latest -f DockerfilesDockerfile.jekyll .
+docker build -t argmin-gravitas -f DockerfilesDockerfile.jekyll .
 docker run -p 4000:4000 -v $(pwd):/srv/jekyll argmin-gravitas/latest
