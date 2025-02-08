e812f08d2b4d7026bd79e124815a42f844d889eb
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 17:52:29 +0000

    Serve script fails on error

--- scripts/serve-docker.sh
+++ scripts/serve-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
 # Make sure this script is run from the project root.
 
-docker build -t argmin-gravitas -f DockerfilesDockerfile.jekyll .
+docker build -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
 docker run -p 4000:4000 -v $(pwd):/srv/jekyll argmin-gravitas/latest
