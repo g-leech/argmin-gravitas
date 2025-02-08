c71a05adf8143f23243232efc3527c4687c259c3
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 17:50:50 +0000

    Add Dockerfile and serve script

---
+++ scripts/serve-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Make sure this script is run from the project root.
+
+docker build -t argmin-gravitas/latest -f DockerfilesDockerfile.jekyll .
+docker run -p 4000:4000 -v $(pwd):/srv/jekyll argmin-gravitas/latest
