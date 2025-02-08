d774553a41c8f9520b880329b07469f4d50cd415
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 21:19:24 +0000

    Tidy up

--- scripts/dev-server2.sh
+++ scripts/dev-server2.sh
@@ -6,4 +6,5 @@ set -e
 echo "Building Docker image..."
 docker build --platform=linux/amd64 -t argmin-gravitas/jekyll2 -f Dockerfiles/Dockerfile.jekyll2 .
 echo "Running Docker container..."
-docker run --platform=linux/amd64  -p 4000:4000 argmin-gravitas/jekyll2
+# docker run --platform=linux/amd64  -p 4000:4000 argmin-gravitas/jekyll2
+docker run --platform=linux/amd64 argmin-gravitas/jekyll2
