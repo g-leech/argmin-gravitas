229d1a9bcd9f3efa0677e1bfa2a88b0cc5a92710
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 20:11:55 +0000

    Bundle update

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -4,6 +4,6 @@ set -e
 # Make sure this script is run from the project root.
 
 echo "Building Docker image..."
-docker build --progress=plain --platform=linux/amd64 -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
+docker build --platform=linux/amd64 -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
 echo "Running Docker container..."
 docker run --platform=linux/amd64  -p 4000:4000 argmin-gravitas/latest
