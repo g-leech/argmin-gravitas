951ea053890a4857c82b6b4ac772cdd6612be402
John Morrice <john.morrice.developer@gmail.com>
2025-02-08 21:26:45 +0000

    Set up dev server for live editing

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Make sure this script is run from the project root.
+echo "Building Docker image for Jekyll development..."
+docker build --platform=linux/amd64 -t argmin-gravitas/jekyll-dev -f Dockerfiles/Dockerfile.jekyll .
 
-echo "Building Docker image..."
-docker build --platform=linux/amd64 -t argmin-gravitas/latest -f Dockerfiles/Dockerfile.jekyll .
-echo "Running Docker container..."
-docker run --platform=linux/amd64  -p 4000:4000 argmin-gravitas/latest
+echo "Running Docker container with live file editing enabled..."
+# The -v flag mounts the current directory (source code) into /srv/jekyll in the container.
+docker run --platform=linux/amd64 -p 4000:4000 -v "$(pwd)":/srv/jekyll argmin-gravitas/jekyll-dev
