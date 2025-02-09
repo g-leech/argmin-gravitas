146525ca36485c21d84fa69b8ae9e2a3026347d1
John Morrice <john.morrice.developer@gmail.com>
2025-02-09 13:30:55 +0000

    Remove AMD64 platform from docker

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Building Docker image for Jekyll development..."
-docker build --platform=linux/amd64 -t argmin-gravitas/jekyll-dev -f Dockerfiles/Dockerfile.jekyll .
+docker build -t argmin-gravitas/jekyll-dev -f Dockerfiles/Dockerfile.jekyll .
 
 echo "Running Docker container with live file editing enabled..."
 # The -v flag mounts the current directory (source code) into /srv/jekyll in the container.
-docker run --platform=linux/amd64 -p 4000:4000 -v "$(pwd)":/srv/jekyll argmin-gravitas/jekyll-dev
+docker run -p 4000:4000 -v "$(pwd)":/srv/jekyll argmin-gravitas/jekyll-dev
