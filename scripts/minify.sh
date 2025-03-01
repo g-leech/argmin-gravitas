e1acb067ad15b394a334175e9c746b2fb3b45648
John Morrice <john.morrice.developer@gmail.com>
2025-03-01 18:37:24 +0000

    Exclude /files from minify

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -8,7 +8,7 @@ du -sh _site
 docker run --rm --platform linux/amd64 \
   -v "$(pwd)/_site":/srv \
   -w /srv \
-  tdewolff/minify sh -c 'mkdir -p /tmp/minify && minify --exclude -r -o /tmp/minify . && cp -a /tmp/minify/. . && rm -rf /tmp/minify'
+  tdewolff/minify sh -c 'mkdir -p /tmp/minify && minify  --exclude "files/**" -r -o /tmp/minify . && cp -a /tmp/minify/. . && rm -rf /tmp/minify'
 
 
 echo "Size after HTML/CSS/JS minification:"
