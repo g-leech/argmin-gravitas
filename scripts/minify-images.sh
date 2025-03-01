fa3cd264903ade102cb563224f2090fb0aff87a7
John Morrice <john.morrice.developer@gmail.com>
2025-03-01 19:38:52 +0000

    Scripts to scale images

--- scripts/minify-images.sh
+++ scripts/minify-images.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Build imagemin Dockerfile
-docker build --platform linux/amd64 --progress plain -t argmin-gravitas/imagemagick -f Dockerfiles/Dockerfile.imagemagick .
+docker build --platform linux/amd64 --progress plain -t argmin-gravitas/imagemogrify -f Dockerfiles/Dockerfile.imagemogrify .
 
 echo "Size before image minification"
 du -sh img
 
 # Run the image minification command
-docker run --rm --platform linux/amd64 -v "$(pwd)/img":/srv argmin-gravitas/imagemagick
+docker run --rm --platform linux/amd64 -v "$(pwd)/img":/srv argmin-gravitas/imagemogrify
 
 echo "Size after image minification"
 du -sh img
